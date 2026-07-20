ee162437ab6586787ef795301c610ce625b59aad
WesleyPenteado <wesleycosta.ne@gmail.com>
2026-07-20 15:22:23 -0700

    Inlcuir BP_Model

--- src/models.py
+++ src/models.py
@@ -46,3 +46,24 @@ class DFC_Model(dfc_base):
     VL_CONTA = Column(Numeric(20, 4), nullable=False)
     ST_CONTA_FIXA = Column(String(1), nullable=False)  # "S" ou "N"
     ANO = Column(Integer, nullable=True)
+
+class BP_Model(bp_base):
+    __tablename__ = "bp"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    CNPJ_CIA = Column(String, nullable=False)
+    DT_REFER = Column(Date, nullable=True)
+    VERSAO = Column(Integer, nullable=False)
+    DENOM_CIA = Column(String, nullable=False)
+    CD_CVM = Column(Integer, nullable=False)
+    GRUPO_DFP = Column(String, nullable=False)
+    MOEDA = Column(String, nullable=False)
+    ESCALA_MOEDA = Column(String, nullable=True)
+    ORDEM_EXERC = Column(String, nullable=True)
+    DT_FIM_EXERC = Column(Date, nullable=True)
+    CD_CONTA = Column(String, nullable=False)
+    DS_CONTA = Column(String, nullable=False)
+    VL_CONTA = Column(Numeric(20, 4), nullable=False)
+    ST_CONTA_FIXA = Column(String(1), nullable=False)  # "S" ou "N"
+    ANO = Column(Integer, nullable=True)
