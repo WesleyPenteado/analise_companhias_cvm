2c7de72c22619ab489fad9ce075ef4c78be2a4ea
WesleyPenteado <wesleycosta.ne@gmail.com>
2026-07-20 15:24:25 -0700

    Incluir validador_df_bp

--- src/validator.py
+++ src/validator.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
-from src.schema import DRE_Schema, DFC_Schema
+from src.schema import DRE_Schema, DFC_Schema, BP_Schema
 
 
 def validador_df_DRE(df) -> Tuple[List[DRE_Schema], List[dict]]:
@@ -30,6 +30,25 @@ def validador_df_DFC(df) -> Tuple[List[DFC_Schema], List[dict]]:
     for i, row in df.iterrows():
         try:
             valid = DFC_Schema(**row.to_dict())
+            valid_rows.append(valid)
+
+        except Exception as e:
+            errors.append({
+                "index": i,
+                "error": str(e),
+                "row": row.to_dict()
+            })
+
+    return valid_rows, errors
+
+def validador_df_bp(df) -> Tuple[List[BP_Schema], List[dict]]:
+    '''Valida o DataFrame de BP usando o Pydantic. Retorna uma lista de linhas válidas e uma lista de erros encontrados.'''
+    valid_rows = []
+    errors = []
+
+    for i, row in df.iterrows():
+        try:
+            valid = BP_Schema(**row.to_dict())
             valid_rows.append(valid)
 
         except Exception as e:
