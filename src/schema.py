29297a8d1a2986bcbf2d6390fbd2af052354bd1f
WesleyPenteado <wesleycosta.ne@gmail.com>
2026-07-20 15:20:50 -0700

    Incluir BP_Schema

--- src/schema.py
+++ src/schema.py
@@ -43,4 +43,22 @@ class DFC_Schema(BaseModel):
     ANO: Optional[int]
 
 
+class BP_Schema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    CNPJ_CIA: str
+    DT_REFER: Optional[date]
+    VERSAO: int
+    DENOM_CIA: str
+    CD_CVM: int
+    GRUPO_DFP: str
+    MOEDA: str
+    ESCALA_MOEDA: Optional[str]
+    ORDEM_EXERC: Optional[str]
+    DT_FIM_EXERC: Optional[date]
+    CD_CONTA: str
+    DS_CONTA: str
+    VL_CONTA: Decimal
+    ST_CONTA_FIXA: Literal["S", "N"]
+    ANO: Optional[int]
+
 
